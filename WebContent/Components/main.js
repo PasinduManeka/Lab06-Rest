2b0d7f4c062761eea05c7fa01dec5e7ced38afa7
PasinduManeka <manekaherat815@gmail.com>
2021-05-07 15:22:42 +0530

    save button handler

--- WebContent/Components/main.js
+++ WebContent/Components/main.js
@@ -4,10 +4,34 @@ $(document).ready(function()
 	$("#alertError").hide();
 });
 
+//Save -------
 $(document).on("click","#btnSave",function(event){
 	//Clear status messages
 	$("#alertSuccess").text("");
 	$("#alertSuccess").hide();
 	$("#alertError").text("");
 	$("#alertError").hide();
+	
+	//Form validation 
+	var status = validateItem();
+	
+	//If not valid
+	if(status != true)
+		{
+			$("#alertError").text(status);
+			$("#alertError").show();
+			return;
+		}
+	
+	var student = etStudentCard($("#txtName").val().trim(),
+			$('input[name="rdoGender"]:checked').val(),
+			$("#ddlyear").val());
+	
+	$("#colStudents").append(student);
+	
+	$("#alertSuccess").text("Saved Successfully.");
+	$("#alertSuccess").hide();
+	
+	$("#formStudent")[0].reset();
+		
 });
