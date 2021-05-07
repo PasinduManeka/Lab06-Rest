3f5adafb36c7bc4f3da57b60defdc3f93e389a48
PasinduManeka <manekaherat815@gmail.com>
2021-05-07 19:43:06 +0530

    set delete function

--- WebContent/Components/main.js
+++ WebContent/Components/main.js
@@ -56,3 +56,60 @@ function validateItemForm()
 	
 	return true;
 }
+
+//Generate the student cart
+
+function getStudent(name, gender, year)
+{
+	var student = "";
+	
+	var title = (gender=="Male")?"Mr.":"Ms.";
+	
+	var yearNumber = "";
+	
+	switch(year){
+		case "1":
+			yearNummer="1st";
+			break;
+		case "2":
+			yearNumber ="2nd";
+			break;
+		case "3":
+			yearNumber="3rd";
+			break;
+		case "4":
+			yearNumber="4th";
+			break;
+	}
+	
+	student += "<div class=\"student card bg-light m-2\" " +
+			"style=\"max-width:10rem; float: left;\">";
+	student += "<div class=\"card-body\">";
+	student += title+ " "+name+",";
+	student += "<br>";
+	student += yearNumber + "year";
+	student += "</div>";
+	student +="<input type=\"button\" value=\"Remove\" class=\"btn btn-danger remove\">";
+	student +="</div>";
+	
+	return student;
+}
+
+//remove button handler
+$(documnent).on("click", ".remove", function(event)
+{
+	$(this).closest(".student").remove();
+	
+	$("#alertSuccess").text("Removed successfully.");
+	$("#alertSuccess").show();
+	
+})
+
+
+
+
+
+
+
+
+
